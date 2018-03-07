162dde51c1bc8dc368788cc2af872cc82bc2c9ce
WOOSUB LEE <dntjqdlekd@gmail.com>
2018-03-08 00:30:49 +0900

    utf-8

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module Apply2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.use PDFKit::Middleware, :print_media_type => true
+    config.middleware.use PDFKit::Middleware, :print_media_type => true, :encoding => 'UTF-8'
   end
 end
